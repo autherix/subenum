af609936adb4e29da8fab66ba2ef4ee3aa3f0ee0
autherix <105848507+Autherix@users.noreply.github.com>
2023-06-15 07:27:22 +0000

    Added subenum-all-bc + fix on wildscope + updated subenum for RAW target mode

--- .bin/subenum-all-bc-targets.sh
+++ .bin/subenum-all-bc-targets.sh
@@ -6,8 +6,8 @@ c_path=$(dirname $0)
 # Make a logs directory in current running file's directory
 mkdir -p $c_path/logs
 
-# In two upper directory, read the file h1_tgts_new_full.json , then jq it to get the list of targets' handles
-target_handle_list=$(healerdb h1_targetinfo list -db bbplats -coll h1 -j | jq -r '.result[]')
+# In two upper directory, read the file bc_tgts_new_full.json , then jq it to get the list of targets' handles
+target_handle_list=$(healerdb bc_targetinfo list -db bbplats -coll bc -j | jq -r '.result[]')
 
 # Iterate over the list of targets' handles and run subenum on each of them
 for target_handle in $target_handle_list
@@ -17,16 +17,16 @@ do
     # Print the target handle
     echo "Target handle: $target_handle"
     # Run subenum on the target handle
-    # subenum -p h1 -db enum -t $target_handle
-    subenum -p h1 -db enum -t $target_handle >> $c_path/logs/subenum-all-h1.log 2>&1
+    # subenum -p bc -db enum -t $target_handle
+    subenum -p bc -db enum -t $target_handle > $c_path/logs/subenum-all-bc.log 2>&1
     wait $!
     # Check if the subenum command was successful or not
     if [ $? -eq 0 ]
     then
         echo "----------------------------------------"
     else
-        echo "subenum failed on target: $target_handle" >> $c_path/logs/subenum-all-h1-targets.log
-        echo "Error text: $?" >> $c_path/logs/subenum-all-h1-targets.log
+        echo "subenum failed on target: $target_handle" >> $c_path/logs/subenum-all-bc-targets.log
+        echo "Error text: $?" >> $c_path/logs/subenum-all-bc-targets.log
         echo "----------------------------------------"
     fi
 done
